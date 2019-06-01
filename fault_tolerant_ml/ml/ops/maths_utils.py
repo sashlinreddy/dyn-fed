9881930624d357d3c937ecdd7bf95113baec9ad4
Sashlin Reddy <sashr07@gmail.com>
2019-06-01 19:33:12 +0200

    Add linspace approx and reconstruction

--- fault_tolerant_ml/ml/ops/maths_utils.py
+++ fault_tolerant_ml/ml/ops/maths_utils.py
@@ -6,3 +6,44 @@ def sigmoid(s):
     """
     # print(s)
     return 1./(1. + np.exp(-s))
+
+def linspace_quantization(matrix, interval=8):
+    """Quantizes parameters
+
+    Arguments:
+        theta (np.ndarray): Parameter matrix to be quantized
+
+    Returns:
+        msg (np.ndarray): Structured numpy array that is quantized
+
+    """
+    min_theta_val = matrix.min() + 1e-8
+    max_theta_val = matrix.max() + 1e-8
+    bins = np.linspace(min_theta_val, max_theta_val, interval)
+    theta_bins = np.digitize(matrix, bins).astype(np.int8)
+
+    struct_field_names = ["min_val", "max_val", "interval", "bins"]
+    struct_field_types = [np.float32, np.float32, np.int32, 'b']
+    struct_field_shapes = [1, 1, 1, (matrix.shape)]
+
+    msg = np.zeros(1, dtype=(list(zip(struct_field_names, struct_field_types, struct_field_shapes))))
+    msg[0] = (min_theta_val, max_theta_val, interval, theta_bins)
+
+    return msg
+
+def reconstruct_approximation(buf, shape, r_dtype=np.float32):
+    # Reconstruct theta matrix from min, max, no. of intervals and which bins
+    # each parameter value falls in
+    struct_field_names = ["min_val", "max_val", "interval", "bins"]
+    struct_field_types = [np.float32, np.float32, np.int32, 'b']
+    struct_field_shapes = [1, 1, 1, (shape)]
+    dtype=(list(zip(struct_field_names, struct_field_types, struct_field_shapes)))
+                                    
+    data = np.frombuffer(buf, dtype=dtype)
+    min_theta_val, max_theta_val, interval, matrix_bins = data[0]
+
+    # Generate lineared space vector
+    bins = np.linspace(min_theta_val, max_theta_val, interval, dtype=r_dtype)
+    matrix = bins[matrix_bins].reshape(shape)
+
+    return matrix
