e8cc74ad155445478fa432cb78f929ffbf56fcbf
Sashlin Reddy <sashr07@gmail.com>
2019-07-24 21:08:00 +0200

    Moved ops to root

--- fault_tolerant_ml/losses/loss_fns.py
+++ fault_tolerant_ml/losses/loss_fns.py
@@ -1,4 +1,98 @@
 import numpy as np
+from abc import ABC, abstractmethod
+
+class Loss(ABC):
+    """Base class for all loss functions
+    """
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    @abstractmethod
+    def loss(self, y, y_pred):
+        raise NotImplementedError
+
+    @abstractmethod
+    def grad(self, y, y_pred):
+        raise NotImplementedError
+
+class CrossEntropyLoss(Loss):
+    """Cross Entropy loss
+
+    loss = -y * np.log(y_pred) - (1 - y) * np.log(1 - y_pred)    
+    """
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def loss(y, y_pred, reduce=True):
+        """Returns cross entropy loss for each sample or across batch
+
+        y (numpy.ndarray): Actual labels
+        y_pred (numpy.ndarray): Predicted labels
+        reduce (bool): Whether or not to return the average across the batch
+
+        Returns:
+            loss (float/numpy.ndarray): If reduce, then will return a float else a numpy array
+        """
+        loss = -y * np.log(y_pred) - (1 - y) * np.log(1 - y_pred)
+        if reduce:
+            loss = np.mean(loss)
+        else:
+            return loss
+
+    @staticmethod
+    def grad(y, y_pred):
+        """Returns gradient for cross entropy loss
+
+        y (numpy.ndarray): Actual labels
+        y_pred (numpy.ndarray): Predicted labels
+
+        Returns:
+            grad (numpy.ndarray): Gradient tensor
+        """
+        grad = y_pred - y
+
+        return grad
+
+class MSELoss(Loss):
+    """Mean squared error
+    """
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def loss(y, y_pred, reduce=True):
+        """Returns mean squared error loss for each sample or across batch
+
+        y (numpy.ndarray): Actual labels
+        y_pred (numpy.ndarray): Predicted labels
+        reduce (bool): Whether or not to return the average across the batch
+
+        Returns:
+            loss (float/numpy.ndarray): If reduce, then will return a float else a numpy array
+        """
+        loss = (1 / 2) * np.linalg.norm(y_pred - y) ** 2
+        if reduce:
+            loss = np.mean(loss)
+        else:
+            return loss
+
+    @staticmethod
+    def grad(y, y_pred, z, act_fn):
+        """Returns gradient for mean squared error loss for each sample or across batch
+
+        y (numpy.ndarray): Actual labels
+        y_pred (numpy.ndarray): Predicted labels
+        z (numpy.ndarray): Pre non-linearity
+        act_fn (fault_tolerant_ml.activations.Activation): The activation function for the output layer of the network
+
+        Returns:
+            grad (numpy.ndarray): Gradient tensor
+        """
+        grad = (y_pred - y) * act_fn.grad(z)
+
+        return grad
+
 
 # Logistic regression
 def single_cross_entropy_loss(y_pred, y):
