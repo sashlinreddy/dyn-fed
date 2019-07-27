a01f8d04c3010ba96fb51eaa631b90a551a5ebe9
Sashlin Reddy <675723@students.wits.ac.za>
2019-07-27 16:05:34 +0200

    Changed directory for running executor file

--- slurm_launch.sh
+++ slurm_launch.sh
@@ -3,7 +3,8 @@
 #SBATCH -o /home-mscluster/sreddy/logs/slurm/slurm_%j.log
 #SBATCH -t 10:00
 #SBATCH -p batch
-#SBATCH --export=LOGDIR=/home-mscluster/sreddy/logs/slurm,TFDIR=/home-mscluster/sreddy/logs/,FIGDIR=/home-mscluster/sreddy/fault-tolerant-ml/reports/figures,PROJECT_DIR=/home-mscluster/sreddy/fault-tolerant-ml
+#SBATCH --export=LOGDIR=/home-mscluster/sreddy/logs/slurm,FIGDIR=/home-mscluster/sreddy/fault-tolerant-ml/reports/figures,PROJECT_DIR=/home-mscluster/sreddy/fault-tolerant-ml
+# ,TFDIR=/home-mscluster/sreddy/logs/
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -20,9 +21,9 @@ while [ "$1" != "" ]; do
 done
 
 export PYTHON_EXE=/home-mscluster/sreddy/miniconda3/envs/ftml/bin/python3
-export MASTER_EXE=/home-mscluster/sreddy/fault-tolerant-ml/tests/run_master.py
-export WORKER_EXE=/home-mscluster/sreddy/fault-tolerant-ml/tests/run_worker.py
-export DATA_DIR=/home-mscluster/sreddy/fault-tolerant-ml/data
+export MASTER_EXE=/home-mscluster/sreddy/fault-tolerant-ml/scripts/run_master.py
+export WORKER_EXE=/home-mscluster/sreddy/fault-tolerant-ml/scripts/run_worker.py
+export DATA_DIR=/home-mscluster/sreddy/fault-tolerant-ml/data/fashion-mnist
 
 echo -e '0\t' $PYTHON_EXE $MASTER_EXE $SLURM_NTASKS $verbose > /home-mscluster/sreddy/fault-tolerant-ml/m_w_$SLURM_JOBID.conf
 echo -e '*\t' $PYTHON_EXE $WORKER_EXE $SLURM_NTASKS $verbose -i %t >> /home-mscluster/sreddy/fault-tolerant-ml/m_w_$SLURM_JOBID.conf
