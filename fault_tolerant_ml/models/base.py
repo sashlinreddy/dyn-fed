0cba443b9848538ff682fff3ff1bba387a11a46f
Sashlin Reddy <sashr07@gmail.com>
2019-09-02 21:11:55 +0200

    Linting

--- fault_tolerant_ml/models/base.py
+++ fault_tolerant_ml/models/base.py
@@ -1,12 +1,17 @@
+"""Base class for models
+"""
+from __future__ import absolute_import, division, print_function
+
 import logging
 
-from fault_tolerant_ml.metrics import accuracy_score
 from fault_tolerant_ml.distribute.strategy import LocalStrategy
+from fault_tolerant_ml.metrics import accuracy_score
 
-class BaseEstimator(object):
 
+class BaseEstimator():
+    """Base Estimator
+    """
     def __init__(self, optimizer, strategy=None, **kwargs):
-        
         self.encode_name = None
         if "encode_name" in kwargs:
             self.encode_name = kwargs["encode_name"]
@@ -19,19 +24,24 @@ class BaseEstimator(object):
 
         self._logger = logging.getLogger(f"ftml.models.{self.__class__.__name__}")
 
-class ClassifierMixin(object):
-
+class ClassifierMixin():
+    """ClassiferMixin for classfication models
+    """
     def __init__(self):
         
         self._logger = logging.getLogger(f"ftml.models.{self.__class__.__name__}")
 
     def score(self, X, y):
+        """Returns score of corresponding model given X and y
+        """
         return accuracy_score(y, self.predict(X))
 
-class RegressorMixin(object):
-
+class RegressorMixin():
+    """RegressorMixin for regression models
+    """
     def __init__(self):
         pass
 
     def score(self, X, y):
-        pass
+        """Returns score of coressponding model given X and y
+        """
