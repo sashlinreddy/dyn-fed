d900c6765e70393597b8764d52ed9342affd0f68
Sashlin Reddy <sashr07@gmail.com>
2019-09-28 21:05:49 +0200

    Sorted init imports

--- fault_tolerant_ml/distribute/__init__.py
+++ fault_tolerant_ml/distribute/__init__.py
@@ -1,8 +1,12 @@
-from .watch_dog import WatchDog
+"""Distribute module
+"""
 from .coordinator import Coordinator
+from .watch_dog import WatchDog
 from .master import Master
-from .worker import Worker
+# from .masterv2 import Master as MasterV2
 from .strategy import DistributionStrategy, MasterWorkerStrategy
+from .worker import Worker
+# from .workerv2 import Worker as WorkerV2
 
 __all__ = [
     "WatchDog", "Master", "Worker", "DistributionStrategy", "MasterWorkerStrategy"
