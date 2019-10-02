55fccb00dfaf94700fee928398e1d4ab1ab7f608
Sashlin Reddy <sashr07@gmail.com>
2019-10-02 17:40:59 +0200

    Passing verbose to workers

--- scripts/worker_local.sh
+++ scripts/worker_local.sh
@@ -7,7 +7,7 @@ while [ "$2" != "" ]; do
         -v| --VERBOSE )         shift
                                 VERBOSE="-v $2"
                                 ;;
-        -m| --MODEL )         shift
+        -m| --MODEL )           shift
                                 MODEL="$2"
                                 ;;
         -h | --help )           usage
@@ -22,14 +22,14 @@ done
 # Decide on the type of model
 if [ "$MODEL" == "LINEAR" ]; then
     echo "Running Linear Model"
-    pythonw examples/train_linear.py $NWORKERS -r worker -i $TMUX_PANE -t 1
+    pythonw examples/train_linear.py $NWORKERS -r worker $VERBOSE -i $TMUX_PANE -t 1
 elif [ "$MODEL" == "LOGISTIC" ]; then
     echo "Running Logistic Model"
-    pythonw examples/train_logistic.py $NWORKERS -r worker -i $TMUX_PANE -t 1
+    pythonw examples/train_logistic.py $NWORKERS -r worker $VERBOSE -i $TMUX_PANE -t 1
 elif [ "$MODEL" == "LOG2" ]; then
     echo "Running Logistic Model"
-    pythonw examples/train_logisticv2.py $NWORKERS -r worker -i $TMUX_PANE -t 1
+    pythonw examples/train_logisticv2.py $NWORKERS -r worker $VERBOSE -i $TMUX_PANE -t 1
 elif [ "$MODEL" == "NN" ]; then
     echo "Running NN Model"
-    pythonw examples/train_nn.py $NWORKERS -r worker -i $TMUX_PANE -t 1
+    pythonw examples/train_nn.py $NWORKERS -r worker -i $VERBOSE $TMUX_PANE -t 1
 fi
