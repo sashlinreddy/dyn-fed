d3a4456ab3c213bdbd04b1df735841adf60dc71c
Sashlin Reddy <sashr07@gmail.com>
2020-02-25 20:51:36 +0200

    Updated details to setup environment

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -36,9 +36,17 @@ mkdir protoc-3.10.1 && cd protoc-3.10.1
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.10.1/protoc-3.10.1-linux-x86_64.zip
 unzip protoc-3.10.1-linux-x86_64.zip
 
+./fault-tolerant-ml/scripts/compile_proto.sh
+
 echo 'export PROTOBUF_HOME=$HOME/protoc-3.10.1/' >> ~/.bashrc
 echo 'export PATH=${PROTOBUF_HOME}/bin:${PATH}' >> ~/.bashrc
 
 # scp gcp
 # gcloud compute scp data/mnist/*.gz g1-login1:/home/g675723_students_wits_ac_za/fault-tolerant-ml/data/mnist
 # gcloud compute scp data/fashion-mnist/*.gz g1-login1:/home/g675723_students_wits_ac_za/fault-tolerant-ml/data/fashion-mnist
+
+# git config --global user.email "sashr07@gmail.com"
+# git config --global user.name "Sashlin Reddy"
+
+# mkdir -p logs/slurm
+
